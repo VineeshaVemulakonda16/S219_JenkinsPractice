4266de1d554103b8661a3880c53a915e4d09373e
VEMULAKONDA NAAGA DHANYA HIMA VINEESHA <2300031234@kluniversity.in>
2025-08-04 10:38:59 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-    <BrowserRouter basename="/ecommerce">
+   <Router basename="/ecomJenkinsPractice">
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
